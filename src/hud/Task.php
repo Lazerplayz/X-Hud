15ed089abc59a7d7b68c88e9b0b5a5f947b93ad0
Fury <xfurymcpe@gmail.com>
2016-08-08 19:09:59 -0500

    Update Task.php

--- src/hud/Task.php
+++ src/hud/Task.php
@@ -9,34 +9,29 @@ use pocketmine\Server;
 use pocketmine\Player;
 use pocketmine\utils\Config;
 
-Class Task extends PluginTask{
+class Task extends PluginTask{
 
-    const COUNT = 0;
+	const COUNT = 0;
 
-    public function __construct($plugin)
-    {
-        $this->plugin = $plugin;
-        parent::__construct($plugin);
-        $this->count = Task::COUNT;
-            
-    }
-public function onRun($currentTick)
-{
-    if($this->count <= $this->plugin->count){
-    foreach ($this->getOwner()->getServer()->getOnlinePlayers() as $p){
-        if($this->plugin->isHudOn($p) == true){
-        $message = $this->plugin->getMessage($this->count, $p);
-        $p->sendTip($message);
-       }
-    }
-    }
-       
-                                if($this->count < $this->plugin->count){
-					$this->count++;
-                                }
-                                if($this->count == $this->plugin->count){
-					$this->count-=$this->plugin->count;
-                                }
+	public function __construct($plugin){
+        	$this->plugin = $plugin;
+        	parent::__construct($plugin);
+        	$this->count = Task::COUNT;
+	}
+	public function onRun($currentTick){
+		if($this->count <= $this->plugin->count){
+			foreach($this->getOwner()->getServer()->getOnlinePlayers() as $p){
+				if($this->plugin->isHudOn($p) == true){
+					$message = $this->plugin->getMessage($this->count, $p);
+					$p->sendTip($message);
+				}
+			}
+		}
+                if($this->count < $this->plugin->count){
+			$this->count++;
+                }
+                if($this->count == $this->plugin->count){
+			$this->count-=$this->plugin->count;
+                }
         }
-
 }
